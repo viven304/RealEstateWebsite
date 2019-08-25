4077b1e57dc8b319bf93bef7a3945e3dd2ea2ca4
Viven Iyer <36834600+viven304@users.noreply.github.com>
2019-08-25 16:46:33 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+function reduceOpacityOne() {
+    $(function() {
+        $("#one").animate({
+            opacity: '-=0.5'
+        });
+     });
+}
+
+
+function increaseOpacityOne() {
+ $(function() {
+    $("#one").animate({
+        opacity: '+=0.5'
+    });
+ });
+}
+
+
+function reduceOpacityTwo() {
+    $(function() {
+        $("#two").animate({
+            opacity: '-=0.5'
+        });
+     });
+}
+
+
+function increaseOpacityTwo() {
+ $(function() {
+    $("#two").animate({
+        opacity: '+=0.5'
+    });
+ });
+}
+
+
+
+function reduceOpacityThree() {
+    $(function() {
+        $("#three").animate({
+            opacity: '-=0.5'
+        });
+     });
+}
+
+
+function increaseOpacityThree() {
+ $(function() {
+    $("#three").animate({
+        opacity: '+=0.5'
+    });
+ });
+}
+
+
+
+function reduceOpacityFour() {
+    $(function() {
+        $("#four").animate({
+            opacity: '-=0.5'
+        });
+     });
+}
+
+
+function increaseOpacityFour() {
+ $(function() {
+    $("#four").animate({
+        opacity: '+=0.5'
+    });
+ });
+}
